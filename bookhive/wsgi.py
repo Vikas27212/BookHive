fcda9dff334adf9b4cf4ef5f746d980121a6508f
Vikas <x23380322@student.ncirl.ie>
2025-04-01 13:19:53 +0530

    updated code

--- bookhive/wsgi.py
+++ bookhive/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for elibrary project.
+WSGI config for bookhive project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elibrary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookhive.settings')
 
 application = get_wsgi_application()
